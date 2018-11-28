8d8999c3ee7fe142f412f9bc4847e0a16f36a408
dliemohn <44586949+dliemohn@users.noreply.github.com>
2018-11-28 12:07:14 -0500

    Basically Working

--- MyPriorityQueue.java
+++ MyPriorityQueue.java
@@ -2,113 +2,114 @@ import java.util.*;
 
 class MyPriorityQueue<E> implements PriorityQueueADT<E> {
 
-    // you can use either of the following declarations for your heap
-    ArrayList<E> heap;
-    // E[] heap;
-    int iter;
+	// you can use either of the following declarations for your heap
+	ArrayList<E> heap;
+	// E[] heap;
+	int iter;
 
-    Comparator<E> comparator;
+	Comparator<E> comparator;
 
-    MyPriorityQueue(Comparator<E> comparator) {
-        this.comparator = comparator;
-        heap = new ArrayList<E>();
-        // or
-        // heap = (E[]) new Object[/* initial length */];
-        iter = 0;
-    }
+	MyPriorityQueue(Comparator<E> comparator) {
+		this.comparator = comparator;
+		heap = new ArrayList<E>();
+		// or
+		// heap = (E[]) new Object[/* initial length */];
+		iter = 0;
+	}
 
-    public boolean add(E item) {
-        heap.add(item);
-        siftUp(iter++);
-        return true;
-    }
+	public boolean add(E item) {
+		heap.add(item);
+		siftUp(iter++);
+		return true;
+	}
 
-    public E remove() {
-        E result = heap.remove(0);
-        heap.add(0,heap.remove(heap.size()-1));
-        iter--;
-        siftDown(0);
-        return result;
-    }
+	public E remove() {
+		E result = heap.remove(0);
+		iter--;
+		if (!heap.isEmpty()) {
+			heap.add(0, heap.remove(heap.size() - 1));
+			siftDown(0);
+		}
+		return result;
+	}
 
-    public boolean isEmpty() {
-        if (heap.isEmpty())
-            return true;
+	public boolean isEmpty() {
+		if (heap.isEmpty())
+			return true;
 
-        return false;
-    }
+		return false;
+	}
 
-    public int size() {
-        return heap.size();
-    }
+	public int size() {
+		return heap.size();
+	}
 
-    public void clear() {
-        heap.clear();
-    }
+	public void clear() {
+		heap.clear();
+	}
 
-    public String toString() {
-        String result = "";
-        for(E dummy: heap) {
-            result += dummy + " ";
-        }
-        return result;
-    }
+	public String toString() {
+		String result = "";
+		for (E dummy : heap) {
+			result += dummy + " ";
+		}
+		return result;
+	}
 
-    private void siftUp(int pos) {
-        E node = heap.get(pos);
-        int par = parent(pos);
-        if (par != -1) {
-            E parent =  heap.get(par);
-            if (comparator.compare(node, parent) > 0) {
-                heap.set(pos, parent);
-                heap.set(par, node);
-                System.out.println("shifted " + node);
-                siftUp(par);
-            }
-        }
-    }
+	private void siftUp(int pos) {
+		E node = heap.get(pos);
+		int par = parent(pos);
+		if (par != -1) {
+			E parent = heap.get(par);
+			if (comparator.compare(node, parent) > 0) {
+				heap.set(pos, parent);
+				heap.set(par, node);
+				siftUp(par);
+			}
+		}
+	}
 
-    private void siftDown(int pos) {
-        E node = heap.get(pos);
-        int lef = leftChild(pos);
-        int rite = rightChild(pos);
-        if (lef != -1 && rite != -1) {
-            E left = heap.get(lef);
-            E right = heap.get(rite);
-            if (comparator.compare(left, right) > 0) {
-                if (comparator.compare(node, left) <= 0) {
-                    heap.set(pos, left);
-                    heap.set(lef, node);
-                    siftDown(lef);
-                }
-            } else {
-                if (comparator.compare(node, right) <= 0) {
-                    heap.set(pos, right);
-                    heap.set(rite, node);
-                    siftDown(rite);
-                }
-            }
-        }
-    }
+	private void siftDown(int pos) {
+		E node = heap.get(pos);
+		int lef = leftChild(pos);
+		int rite = rightChild(pos);
+		if (lef != -1 && rite != -1) {
+			E left = heap.get(lef);
+			E right = heap.get(rite);
+			if (comparator.compare(left, right) > 0) {
+				if (comparator.compare(node, left) <= 0) {
+					heap.set(pos, left);
+					heap.set(lef, node);
+					siftDown(lef);
+				}
+			} else {
+				if (comparator.compare(node, right) <= 0) {
+					heap.set(pos, right);
+					heap.set(rite, node);
+					siftDown(rite);
+				}
+			}
+		}
+	}
 
-    private int parent(int x) {
-        if (x <= 0)
-            return -1;
-        return (x - 1) / 2;
-    }
+	private int parent(int x) {
+		if (x <= 0)
+			return -1;
+		return (x - 1) / 2;
+	}
 
-    private int leftChild(int x) {
-        int child = (2 * x + 1);
-        if (child >= size())
-            return -1;
-        return child;
-    }
+	private int leftChild(int x) {
+		int child = (2 * x + 1);
+		if (child >= size())
+			return -1;
+		return child;
+	}
 
-    private int rightChild(int x) {
-        int child = (2 * x + 2);
-        if (child >= size())
-            return -1;
-        return child;
-    }
+	private int rightChild(int x) {
+		int child = (2 * x + 2);
+		if (child >= size())
+			return -1;
+		return child;
+	}
 
 }
--- ProcessQueries.java
+++ ProcessQueries.java
@@ -1,45 +1,46 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProcessQueries {
-    public static void main (String[] args) throws IOException {
-        if (args == null)
-            System.out.println("Enter a URL");
-        ArrayList<WebPageIndex> wpis = new ArrayList<WebPageIndex>();
-        File f = new File(args[0]);
-        Scanner urlReader = new Scanner(f);
-        while(urlReader.hasNextLine()) {
-            wpis.add(new WebPageIndex(urlReader.nextLine()));
-        }
-        ArrayList<String> queries = new ArrayList<String>();
-        for( int i = 1; i < args.length; i++) {
-            queries.add(args[i]);
-        }
-        URLComparator engine = new URLComparator(queries);
-        MyPriorityQueue<WebPageIndex> results = new MyPriorityQueue<WebPageIndex>(engine);
-        for(WebPageIndex w: wpis) {
-            results.add(w);
-        }
-        ArrayList<WebPageIndex> holder = new ArrayList<WebPageIndex>();
-        while(!results.isEmpty()) {
-            WebPageIndex top = results.remove();
-            holder.add(top);
-            System.out.println(engine.score(top) + "\t" + top.getUrl());
-        }
-       
-        for(WebPageIndex w: holder)
-            results.add(w);
-       
-        Scanner reader = new Scanner(System.in);
-        String nex = reader.nextLine();
-        while(!nex.equals(""))
-        {
-            wpis.add(new WebPageIndex(nex));
-            results.add(wpis.get(wpis.size()-1));
-            nex = reader.nextLine();
-        }
-       
-    }
+	public static void main(String[] args) throws IOException {
+		try {
+			File f = new File(args[0]);
+		} catch (Exception e) {
+			System.out.println("Enter a file with a list of URLs to search from.");
+			System.exit(-1);
+		}
+		ArrayList<WebPageIndex> wpis = new ArrayList<WebPageIndex>();
+		File f = new File(args[0]);
+		Scanner urlReader = new Scanner(f);
+		while (urlReader.hasNextLine()) {
+			wpis.add(new WebPageIndex(urlReader.nextLine()));
+		}
+		ArrayList<String> queries = new ArrayList<String>();
+		System.out.println("Enter a Query");
+		Scanner input = new Scanner(System.in);
+		String query = input.nextLine();
+		Scanner queryReader = new Scanner(query);
+		while(queryReader.hasNext()) {
+			queries.add(queryReader.next());
+		}
+		System.out.println(query);
+		URLComparator engine = new URLComparator(queries);
+		MyPriorityQueue<WebPageIndex> results = new MyPriorityQueue<WebPageIndex>(engine);
+		for (WebPageIndex w : wpis) {
+			results.add(w);
+		}
+		ArrayList<WebPageIndex> holder = new ArrayList<WebPageIndex>();
+		while (!results.isEmpty()) {
+			WebPageIndex top = results.remove();
+			holder.add(top);
+			System.out.println(engine.score(top) + "\t" + top.getUrl());
+		}
+
+		for (WebPageIndex q : holder)
+			results.add(q);
+
+	}
 }
